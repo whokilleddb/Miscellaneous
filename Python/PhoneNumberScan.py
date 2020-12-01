2edcdf93a20eaed80b6f48e1bb25977e2d1ae57b
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-12-02 03:32:55 +0530

    Update PhoneNumberScan.py

--- Python/PhoneNumberScan.py
+++ Python/PhoneNumberScan.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import phonenumbers
-from phonenumbers import carrier
-from phonenumbers import geocoder
+from phonenumbers import geocoder, carrier
 from tabulate import tabulate
 
 def scanNum(num):
 	number=phonenumbers.parse(num)
-	des=geocode.description_for_number(num,'en')
-	supplier=carrier.name_for_supplier(num,'en')
-	info=[["Country","Supplier"],[des,supplier]]
-	data= str(tabulate(info, headers='firstrow',tablefmt='github'))
-	return data
+	if phonenumbers.is_valid_number(number):
+		des=geocoder.description_for_number(number,'en')
+		supplier=carrier.name_for_number(number,'en')
+		info=[["Number","Country","Supplier"],[num,des,supplier]]
+		data= str(tabulate(info, headers='firstrow',tablefmt='pretty'))
+		return data
+	else :
+		return "[-] Not A Valid Number"
 
 if __name__ == '__main__':
 	num=input("[+] Enter Number : ")
-	print(scanNum(num))
+	print('\n'+scanNum(num))
