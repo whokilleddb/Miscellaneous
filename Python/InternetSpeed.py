f2897be466e49e580aa3ab66b8ecef6b17d97688
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-12-02 03:19:52 +0530

    Update InternetSpeed.py

--- Python/InternetSpeed.py
+++ Python/InternetSpeed.py
@@ -5,26 +5,43 @@ from tabulate import tabulate
 
 class NetSpeed():
 	def __init__(self):
+		#print("[+]  Initialzing Object")
 		self.parser = psutil.net_if_addrs()	
 		self.speed_parser = speedtest.Speedtest()
-		self.interfaces = self.interface()[0]
+		self.interfaces = self.interface()
+		#print("[+] Finished Initialzing Object")
 		
-    def interface(self):
-        interfaces=[]
-        for interface_name, _ in self.parser.items():
-            interfaces.append(str(interface))
-        return interfaces
+	def interface(self):
+		interfaces=[]
+		#print("[+] Detecting Available Interfaces")
+		for interface_name, _ in self.parser.items():
+			#print(f"[+] {interface_name}")
+			interfaces.append(str(interface_name))
+		return interfaces
     
-    def __repr__(self):
-        down=str(f"{round(self.speed_parser.download()/ 1_000_000,2)} Mbps")
-        up=str(f"{round(self.speed_parser.upload()/ 1_000_000,2)} Mbps")
-        interface=self.interfaces
-        data={"Interface" : [interface],
-              "Download" : [down],
-              "Upload" : [up]}
-        table=tabulate(data,headers="keys",tablefmt="pretty")
-        return table
-    
+	def getSpeed(self,inter):
+		down=str(f"{round(self.speed_parser.download()/ 1_000_000,2)} Mbps")
+		up=str(f"{round(self.speed_parser.upload()/ 1_000_000,2)} Mbps")
+		interface=inter
+		return([up,down])
+
+
+	def __repr__(self):
+		#print("[+] Fetching Speed")
+		inter=list()
+		up=list()
+		down=list()
+		for interface in self.interfaces:
+			inter.append(interface)
+			temp=self.getSpeed(interface)
+			up.append(temp[0])
+			down.append(temp[1])
+		data={"Interface" : inter,
+			"Download" : down,
+			"Upload" : up}
+		table=tabulate(data,headers="keys",tablefmt="pretty")
+		return(table)
+
 if __name__ == '__main__':
     print(NetSpeed())
-        
+     
